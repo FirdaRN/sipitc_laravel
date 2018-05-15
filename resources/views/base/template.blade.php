7c327900cc9ae1235abbe9b73291656f01f5193f
Firda Rheinalia N <firdarheinalia@gmail.com>
2018-05-15 23:40:40 +0700

    mengedit link semua page

--- resources/views/base/template.blade.php
+++ resources/views/base/template.blade.php
@@ -32,7 +32,15 @@
     <!-- Navigation -->
  @include('base.navbar')
 
-
+    <!-- Header -->
+    <header class="masthead bg-primary text-white text-center">
+      <div class="container">
+        <img class="img-fluid mb-5 d-block mx-auto" src="{{asset('sipiTC-peminjam/img/profile.png')}}" alt="">
+        <h1 class="text-uppercase mb-0">SipiTC</h1>
+        <hr class="star-light">
+        <h2 class="font-weight-light mb-0">Sistem Informasi Peminjaman Inventaris Laboratorium Infomatika(TC)</h2>
+      </div>
+    </header>
 
     <!-- Portfolio Grid Section -->
     <section class="portfolio" id="portfolio">
