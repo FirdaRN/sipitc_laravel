15b8a205be8046607a4cf8fa233c09f8e94f0a23
Firda Rheinalia N <firdarheinalia@gmail.com>
2018-05-15 23:18:32 +0700

    intial commit

---
+++ routes/web.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+/*Route::get('/', function () {
+    return view('welcome');
+});*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/beranda', function () {
+    return view('admin.beranda');
+});
+
+Route::get('/list', function () {
+    return view('admin.list');
+});
+
+Route::get('/add', function () {
+    return view('admin.add');
+});
+
+Route::get('/history', function () {
+    return view('admin.history');
+});
+
+Route::get('/confirm', function () {
+    return view('admin.konfirm');
+});
+
+Route::get('/home', function () {
+    return view('peminjam.beranda1');
+});
+
+Route::get('/inventaris', function () {
+    return view('peminjam.inventaris');
+});
+
+Route::get('/pinjam', function () {
+    return view('peminjam.pinjam');
+});
+
+Route::get('/kontak', function () {
+    return view('peminjam.kontak');
+});
+
+Route::get('/tentang', function () {
+    return view('peminjam.tentang');
+});
+
